7f286caf13c84ac6b5de683e79a7516087e03798
Daiki-Iijima <daiki.applecreate@gmail.com>
2019-05-26 09:01:03 +0900

    DateManager内にカレンダー用の処理追加

--- CalendarSample/DateManager.swift
+++ CalendarSample/DateManager.swift
@@ -7,6 +7,46 @@
 //
 
 import Foundation
-class Date: NSObject
+
+class DateManager
 {
+    var currentMonthOfDates: [Date] = []   //表記する月の配列
+    var selectedDate = Date()
+    let daysPerWeek: Int = 7
+    var numberOfItems: Int!
+    
+    //日にち数を取得する
+    var daysAcquisition: Int
+    {
+        if let date = firstDateMonth(), let rangeOfWeeks = Calendar.current.range(of: .weekOfMonth, in: .month, for: date)
+        {
+            //  月が持つ週の数
+            let numberOfWeeks = rangeOfWeeks.count
+            
+            //  週の数 * 列の数
+            numberOfItems = numberOfWeeks * daysPerWeek
+            
+            return numberOfItems
+        }
+        
+        return 0
+    }
+    
+    //  月の初めの「年、月、日」を取得
+    func firstDateMonth() -> Date? {
+        
+        //  現在選択されている月の「年、月、日」を取得
+        var components = Calendar.current.dateComponents([.year,.month,.day], from: selectedDate)
+        
+        //  その月の1日を取得
+        components.day = 1
+        
+        //  もし、date型に変換した時にnilではなかった場合にfirstDateMonth(Date型)を返す
+        if let firstDateMonth = Calendar.current.date(from: components)
+        {
+            return firstDateMonth
+        }
+        
+        return nil
+    }
 }
--- CalendarSample/ViewController.swift
+++ CalendarSample/ViewController.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIColor
+{
+    class func lightBlue() ->UIColor
+    {
+        return UIColor(red: 92.0 / 255, green: 192.0 / 255, blue: 210.0 / 255, alpha: 1.0)
+    }
+    
+    class func lightRed() ->UIColor
+    {
+        return UIColor(red: 195.0 / 255, green: 123.0 / 255, blue: 175.0 / 255, alpha: 1.0)
+    }
+}
 
+class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+    
+    
+    let dateManager = DateManager()
+    let daysPerWeek: Int = 7
+    let cellMargin: CGFloat = 2.0
+    var selectedDate = NSDate()
+    var today:NSDate!
+    let weekArray = ["日","月","火","水","木","金","土"]
+    
+    
     @IBOutlet weak var headerBackBtn: UIButton!
     
     @IBOutlet weak var headerNextBtn: UIButton!
@@ -23,6 +45,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    //CollectionViewのセクション数を返す
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        //2つのセクションを作成する
+        return 2;
+    }
+    
+    //セクションに入るcellの個数を返す
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // Section毎にCellの総数を変える.
+        if section == 0 {
+            return 7
+        } else {
+            return dateManager.daysAcquisition
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CalendarCell
+        
+        return cell
     }
 
     @IBAction func TapedHeaderNextBtn(_ sender: Any) {
